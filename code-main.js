3eabdaa9983f1ca3c521ff22b7694cbbb89d51d8
mador-till-prod <61970359+mador-till-prod@users.noreply.github.com>
2021-06-23 10:24:27 +0300

    Add files via upload

---
+++ code-main.js
@@ -0,0 +1,7 @@
+// ספריית אופליין, מאפשרת פתיחה של הדף ללא אינטרנט וללא מחיקה של הקאש כל פעם מחדש
+if ('serviceWorker' in navigator && location.hostname !== 'localhost' && location.hostname !== "127.0.0.1") {
+    let location = ""
+    navigator.serviceWorker.register(location + 'sw.js');
+}
+
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(0(){f 6=[];e.d("6",c.g(6));$(\'.2-8\').3("a",0(){1.4.5="9.7"});$(\'.2-8\').3("j",0(){1.4.5="9.7"});$(\'.2-b\').3("a",0(){$(".l").i();k(0(){1.4.5="b.7"},h)})});',22,22,'function|window|button|on|location|href|arrScore|html|start|explanation|click|about|JSON|setItem|sessionStorage|var|stringify|500|fadeIn|touchstart|setTimeout|black'.split('|'),0,{}))
